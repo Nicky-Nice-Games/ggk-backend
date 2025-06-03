cc9aa795237970c2c4588b90e42a36565ba00253
GrimmKinNovice <77074039+GrimmKinNovice@users.noreply.github.com>
2025-06-03 10:54:02 -0400

    updated player service to include most recent fucntions from the DAO

--- src/main/java/RITIGM/gokartproject/view/gameService/PlayerInfoService.java
+++ src/main/java/RITIGM/gokartproject/view/gameService/PlayerInfoService.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,11 +25,13 @@ public class PlayerInfoService {
     }
 
     /**
-     * 
+     * Retrives player information by ID
+     * @param pid player ID
+     * @return the player corresponding to if succesful 
      */
     @GetMapping("/{pid}")
     public ResponseEntity<PlayerInfo> getPlayerByID(@PathVariable String pid){
-        log.info("GET /playerservice/playerinfo/" + pid);
+        log.info("GET /gameservice/playerinfo/" + pid);
         try{
             PlayerInfo playerInfo = this.playerInfoDAO.getPlayerInfo(pid);
             if(playerInfo != null){
@@ -38,6 +41,77 @@ public class PlayerInfoService {
                  return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
         }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Retrieves player by username
+     * @param username player's username
+     * @param password player's password
+     * @return the player data corresponding to the username and password if successful
+     */
+    @GetMapping("/{username}")
+    public ResponseEntity<PlayerInfo> getPlayerByUsername(@PathVariable String username, @PathVariable String password){
+        log.info("GET /gameservice/playerinfo/" + username);
+        try {
+            PlayerInfo playerInfo = this.playerInfoDAO.getPlayerInfoWithUsername(username, password);
+            if(playerInfo != null){
+                return new ResponseEntity<PlayerInfo>(playerInfo, HttpStatus.OK);
+            }
+            else{
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Creates a new user in the database
+     * @param email user email
+     * @param password user password
+     * @param username user password
+     * @return the new user if they were succesfully created
+     */
+    @PostMapping("")
+    public ResponseEntity<PlayerInfo> createUser(@PathVariable String email, @PathVariable String password, @PathVariable String username ){
+        log.info("POST /gameservice/playerinfo/" + username);
+        try {
+            PlayerInfo new_player = playerInfoDAO.createUser(email, password, username);
+
+            if(new_player != null){
+                return new ResponseEntity<PlayerInfo>(new_player, HttpStatus.OK);
+            }
+            else{
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Overload of user creation that accepts an additional uid parameter
+     * @param email user email
+     * @param password user password
+     * @param uid user id 
+     * @param username username
+     * @return the new user if they were successfully added
+     */
+    @PostMapping("")
+    public ResponseEntity<PlayerInfo> createUser(@PathVariable String email, @PathVariable String password, @PathVariable int uid, @PathVariable String username ){
+        log.info("POST /gameservice/playerinfo/" + username);
+        try {
+            PlayerInfo new_player = playerInfoDAO.createUser(email, password, uid, username);
+
+            if(new_player != null){
+                return new ResponseEntity<PlayerInfo>(new_player, HttpStatus.OK);
+            }
+            else{
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
