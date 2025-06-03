01296872f9d930e854d758187efefdb7ed2de64c
GrimmKinNovice <77074039+GrimmKinNovice@users.noreply.github.com>
2025-06-03 15:58:25 -0400

    finished playerInfoDAO unit tests

--- src/test/java/RITIGM/gokartproject/persistence/gameService/dao/PlayerInfoDAOTest.java
+++ src/test/java/RITIGM/gokartproject/persistence/gameService/dao/PlayerInfoDAOTest.java
@@ -1,5 +1,7 @@
 package RITIGM.gokartproject.persistence.gameService.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -12,6 +14,7 @@ import java.sql.SQLException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import RITIGM.gokartproject.ReflectUtils;
 import RITIGM.gokartproject.model.PlayerInfo;
 
 public class PlayerInfoDAOTest {
@@ -22,42 +25,132 @@ public class PlayerInfoDAOTest {
     public void setupFileDAO(){
         mockConn = mock(Connection.class);
         playerInfoDAO = new PlayerInfoDAO();
+
+        ReflectUtils.setField(this.playerInfoDAO, "conn", mockConn);
     }
 
     @Test
     void testCreateUser() throws SQLException{
         PreparedStatement stmt = mock(PreparedStatement.class);
-        ResultSet check = mock(ResultSet.class);
+        PreparedStatement stmtCheck = mock(PreparedStatement.class);
+        ResultSet resultSet = mock(ResultSet.class);
+        String query = "INSERT INTO players (Email, Password, username) VALUE (?, ?, ?);";
+        String queryLookUp = "SELECT * FROM players WHERE Email = ? AND Password = ? AND username = ?;";
 
-        when(stmt.executeUpdate()).thenReturn(2);
+        when(mockConn.prepareStatement(query)).thenReturn(stmt);
+        when(mockConn.prepareStatement(queryLookUp)).thenReturn(stmtCheck);
+
+        when(stmt.executeUpdate()).thenReturn(1);
+        when(stmtCheck.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(false);
         PlayerInfo player = playerInfoDAO.createUser("test@email.com", "password", "username");
         assertNull(player);
 
         when(stmt.executeUpdate()).thenReturn(1);
-        when(check.next()).thenReturn(false);
+        when(stmtCheck.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.getString("pid")).thenReturn("20");
+        when(resultSet.getString("Email")).thenReturn("test@email.com");
+        when(resultSet.getString("Password")).thenReturn("password");
+        when(resultSet.getInt("uid")).thenReturn(-1);
+        when(resultSet.getString("username")).thenReturn("username");
         player = playerInfoDAO.createUser("test@email.com", "password", "username");
+        assertEquals("20", player.getPid());
+        assertEquals("test@email.com", player.getEmail());
+        assertEquals("password", player.getPw());
+        assertEquals("username", player.getUsername());
+
+
+
+    }
+
+    @Test
+    void testCreateUser2() throws SQLException {
+        PreparedStatement stmt = mock(PreparedStatement.class);
+        PreparedStatement stmtCheck = mock(PreparedStatement.class);
+        ResultSet resultSet = mock(ResultSet.class);
+        String query = "INSERT INTO players (Email, Password, username, uid) VALUE (?, ?, ?, ?);";
+        String queryLookUp = "SELECT * FROM players WHERE Email = ? AND Password = ? AND username = ? AND uid = ?;";
+
+        when(mockConn.prepareStatement(query)).thenReturn(stmt);
+        when(mockConn.prepareStatement(queryLookUp)).thenReturn(stmtCheck);
+
+        when(stmt.executeUpdate()).thenReturn(1);
+        when(stmtCheck.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(false);
+        PlayerInfo player = playerInfoDAO.createUser("test@email.com", "password",1234, "username");
         assertNull(player);
 
         when(stmt.executeUpdate()).thenReturn(1);
-        when(check.next()).thenReturn(false);
+        when(stmtCheck.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.getString("pid")).thenReturn("20");
+        when(resultSet.getString("Email")).thenReturn("test@email.com");
+        when(resultSet.getString("Password")).thenReturn("password");
+        when(resultSet.getInt("uid")).thenReturn(1234);
+        when(resultSet.getString("username")).thenReturn("username");
+        player = playerInfoDAO.createUser("test@email.com", "password",1234, "username");
+        assertEquals("20", player.getPid());
+        assertEquals("test@email.com", player.getEmail());
+        assertEquals("password", player.getPw());
+        assertEquals(1234, player.getUid());
+        assertEquals("username", player.getUsername());
+    }
 
+    @Test
+    void testGetPlayerInfo() throws SQLException{
+        PreparedStatement stmt = mock(PreparedStatement.class);
+        ResultSet resultSet = mock(ResultSet.class);
+        String query = "SELECT * FROM players WHERE pid = ?";
 
+        when(mockConn.prepareStatement(query)).thenReturn(stmt);
+
+        when(stmt.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(false);
+        PlayerInfo player = playerInfoDAO.getPlayerInfo("20");
+        assertNull(player);
+
+        when(stmt.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.getString("pid")).thenReturn("20");
+        when(resultSet.getString("Email")).thenReturn("test@email.com");
+        when(resultSet.getString("Password")).thenReturn("password");
+        when(resultSet.getInt("uid")).thenReturn(1234);
+        when(resultSet.getString("username")).thenReturn("username");
+        player = playerInfoDAO.getPlayerInfo("20");
+        assertEquals("20", player.getPid());
+        assertEquals("test@email.com", player.getEmail());
+        assertEquals("password", player.getPw());
+        assertEquals(1234, player.getUid());
+        assertEquals("username", player.getUsername());
 
     }
 
     @Test
-    void testCreateUser2() {
+    void testGetPlayerInfoWithUsername() throws SQLException {
+        PreparedStatement stmt = mock(PreparedStatement.class);
+        ResultSet resultSet = mock(ResultSet.class);
+        String query = "SELECT * FROM players WHERE username = ? AND Password = ?;";
 
-    }
+        when(mockConn.prepareStatement(query)).thenReturn(stmt);
 
-    @Test
-    void testGetPlayerInfo() {
+        when(stmt.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(false);
+        PlayerInfo player = playerInfoDAO.getPlayerInfoWithUsername("username", "password");
+        assertNull(player);
 
-
-    }
-
-    @Test
-    void testGetPlayerInfoWithUsername() {
-
+        when(stmt.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.getString("pid")).thenReturn("20");
+        when(resultSet.getString("Email")).thenReturn("test@email.com");
+        when(resultSet.getString("Password")).thenReturn("password");
+        when(resultSet.getInt("uid")).thenReturn(1234);
+        when(resultSet.getString("username")).thenReturn("username");
+        player = playerInfoDAO.getPlayerInfoWithUsername("username", "password");
+        assertEquals("20", player.getPid());
+        assertEquals("test@email.com", player.getEmail());
+        assertEquals("password", player.getPw());
+        assertEquals(1234, player.getUid());
+        assertEquals("username", player.getUsername());
     }
 }
