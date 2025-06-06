4c03889177f25130aeadd7b7cd9f2f255b8f09e8
PstormQT <131224632+PstormQT@users.noreply.github.com>
2025-06-05 23:55:31 -0400

    A

---
+++ src/main/java/RITIGM/gokartproject/StreamLambdaHandler.java
@@ -0,0 +1,27 @@
+package RITIGM.gokartproject;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class StreamLambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
+
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(GokartprojectApplication.class);
+        } catch (ContainerInitializationException e) {
+            // If initialization fails, rethrow as a runtime exception
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
+    }
+
+    @Override
+    public AwsProxyResponse handleRequest(AwsProxyRequest input, Context context) {
+        return handler.proxy(input, context);
+    }
+}
