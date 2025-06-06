50f11d33acf46ea689c958f9bbb39d6900111ef8
GrimmKinNovice <77074039+GrimmKinNovice@users.noreply.github.com>
2025-06-06 10:31:59 -0400

    web player info interface filled out

--- src/main/java/RITIGM/gokartproject/persistence/webService/interfaces/WebPlayerInfoInterface.java
+++ src/main/java/RITIGM/gokartproject/persistence/webService/interfaces/WebPlayerInfoInterface.java
@@ -1,5 +1,47 @@
-package RITIGM.gokartproject.persistence.webService.interface;
+package RITIGM.gokartproject.persistence.webService.interfaces;
+
+import java.sql.SQLException;
+
+import RITIGM.gokartproject.model.PlayerInfo;
 
 public interface WebPlayerInfoInterface {
-    
+    /**
+     * 
+     * @param playerID
+     * @return
+     * @throws SQLException
+     */
+    abstract PlayerInfo getPlayerInfo(String playerID) throws SQLException;
+
+    /**
+     * 
+     * @param username
+     * @param pw
+     * @return
+     * @throws SQLException
+     */
+    abstract PlayerInfo getPlayerInfoWithUsername(String username, String pw) throws SQLException;
+
+    /**
+     * 
+     * @param email
+     * @param pw
+     * @param username
+     * @return
+     * @throws SQLException
+     */
+    abstract PlayerInfo createUser(String email, String pw, String username) throws SQLException;
+
+    /**
+     * 
+     * @param email
+     * @param pw
+     * @param uid
+     * @param username
+     * @return
+     * @throws SQLException
+     */
+    abstract PlayerInfo createUser(String email, String pw,int uid, String username) throws SQLException;
+
+
 }
