1ce0679bda1f5c1e0cf8162d85881a3f5207df87
GrimmKinNovice <77074039+GrimmKinNovice@users.noreply.github.com>
2025-06-11 09:53:21 -0400

    racelog tests finished

---
+++ src/test/java/RITIGM/gokartproject/model/LeaderboardDataTest.java
@@ -0,0 +1,60 @@
+package RITIGM.gokartproject.model;
+
+import org.junit.jupiter.api.Test;
+
+public class LeaderboardDataTest {
+    @Test
+    void testGetPid() {
+
+    }
+
+    @Test
+    void testGetRaceID() {
+
+    }
+
+    @Test
+    void testGetRaceTime() {
+
+    }
+
+    @Test
+    void testGetUsername() {
+
+    }
+
+    @Test
+    void testGetraceStartTime() {
+
+    }
+
+    @Test
+    void testSetPid() {
+
+    }
+
+    @Test
+    void testSetRaceID() {
+
+    }
+
+    @Test
+    void testSetRaceTime() {
+
+    }
+
+    @Test
+    void testSetUsername() {
+
+    }
+
+    @Test
+    void testSetraceStartTime() {
+
+    }
+
+    @Test
+    void testToString() {
+
+    }
+}
---
+++ src/test/java/RITIGM/gokartproject/model/responseReceiver/LoginCredsTest.java
@@ -0,0 +1,25 @@
+package RITIGM.gokartproject.model.responseReceiver;
+
+import org.junit.jupiter.api.Test;
+
+public class LoginCredsTest {
+    @Test
+    void testGetPassword() {
+
+    }
+
+    @Test
+    void testGetUsername() {
+
+    }
+
+    @Test
+    void testSetPassword() {
+
+    }
+
+    @Test
+    void testSetUsername() {
+
+    }
+}
